b8088ca4505829162f28db765d966261b49ea32b
JagpreetRandio <115323708+JagpreetRandio@users.noreply.github.com>
2023-04-16 15:18:43 -0700

    server

--- book-search/server/server.js
+++ book-search/server/server.js
@@ -11,7 +11,7 @@ const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
 
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3004;
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
@@ -29,10 +29,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// app.get('*', (req,res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
-
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
