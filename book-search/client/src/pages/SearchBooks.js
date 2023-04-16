47ab49052eb936f70cca7b287018465de2691917
JagpreetRandio <115323708+JagpreetRandio@users.noreply.github.com>
2023-04-16 15:01:47 -0700

    fixed bootstrap issue

--- book-search/client/src/pages/SearchBooks.js
+++ book-search/client/src/pages/SearchBooks.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
-import { SAVE_BOOK } from '../utils/mutations';
-import {useMutation} from '@apollo/react-hooks';
-
 const SearchBooks = () => {
+  const [saveBook] = useMutation(SAVE_BOOK);
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
@@ -16,9 +15,6 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
-
-  const [saveBook, {error}] = useMutation(SAVE_BOOK );
-
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -54,6 +50,7 @@ const SearchBooks = () => {
       setSearchedBooks(bookData);
       setSearchInput('');
     } catch (err) {
+      
       console.error(err);
     }
   };
@@ -63,23 +60,24 @@ const SearchBooks = () => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
- 
-
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
+
 
     if (!token) {
       return false;
     }
 
     try {
-      // const response = await saveBook(bookToSave, token);
-      const {data} = await saveBook({
-        variables: { input: bookToSave }
-      });
-
+      const book = { ...bookToSave}
+      
+      await saveBook({
+        variables: {...book}
+      })
+      console.log(book)
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+
     } catch (err) {
       console.error(err);
     }
