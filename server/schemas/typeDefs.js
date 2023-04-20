70d5ebed050b693596f64735b28bef24705474a8
JagpreetRandio <115323708+JagpreetRandio@users.noreply.github.com>
2023-04-20 01:14:30 -0700

    its working

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -10,6 +10,14 @@ const typeDefs = gql`
         image: String
         link: String
     }
+    input BookInput {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
     type User {
         _id: ID
         username: String
@@ -23,7 +31,7 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(authors: [String], description: String, title: String, bookId: String, image: String, link: String): User
+        saveBook(input: BookInput): User
         removeBook(bookId: String): User
     }
     type Auth {
